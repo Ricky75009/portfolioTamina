98110c59e8443e9f04457603f9a527a92a73ede9
Roman KINDLER <roman@MacBook-Pro-de-Roman.local>
2021-10-14 14:01:09 +0200

    manage to have the shape growing up expensively as I wished

--- static/javascript.js
+++ static/javascript.js
@@ -4,7 +4,8 @@ btn.addEventListener('click', updateBtn);
 function updateBtn() {
     if (btn.value === 'animation') {
       btn.value = 'retour';
-    
+        document.getElementById('mask-svg');
+        element.classList.add("shrinkgrow");
     } else {
       btn.value = 'animation';
     
