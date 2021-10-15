e03dfb8841c1297ee2ff1349ae5918e9cc18ff91
Roman KINDLER <roman@MacBook-Pro-de-Roman.local>
2021-10-15 17:37:10 +0200

    woking more and more on the animation of the shape

--- static/javascript.js
+++ static/javascript.js
@@ -1,15 +1,39 @@
 
 
 
-  var mask1 = document.getElementById('hover-mask-svg1');
+var hover1 = document.getElementById('hover-mask-svg1');
 
-  mask1.style.cursor = 'pointer';
-  mask1.onclick = function() {
-    var element=document.getElementById('mask-svg1');
-    element.classList.add("animation1");
-    console.log('clicked')
-    
 
-  };
+hover1.onclick = function() {
+var element=document.getElementById('mask-svg1');
+
+if (element.classList.contains("animation1")){
+  element.classList.replace("animation1","animation1-reverse");
+}else if (element.classList.contains("animation1-reverse")){
+  element.classList.replace("animation1-reverse","animation1");
+      }else{
+  element.classList.add("animation1");
+}
+
+console.log('clicked')
+};
+
+
+var mask1 = document.getElementById('mask-svg1');
+mask1.style.cursor = 'pointer';
+mask1.onclick = function() {
+var element=document.getElementById('mask-svg1');
+
+if (element.classList.contains("animation1")){
+  element.classList.replace("animation1","animation1-reverse");
+}else if (element.classList.contains("animation1-reverse")){
+  element.classList.replace("animation1-reverse","animation1");
+      }else{
+  element.classList.add("animation1");
+}
+
+console.log('clicked')
+};
+
 
 
