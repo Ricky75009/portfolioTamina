06683e9e121b5798a86691c548f41de318abd0d9
Roman KINDLER <roman@MacBook-Pro-de-Roman.local>
2021-10-20 19:24:06 +0200

    working on smoothing the animation features (cursor, hover, etc)

--- static/javascript.js
+++ static/javascript.js
@@ -1,23 +1,36 @@
 
 
 
-var hover1 = document.getElementById('hover-mask-svg1');
+var hovermasksvg1 = document.getElementById('hover-mask-svg1');
 
 
-hover1.onclick = function() {
+hovermasksvg1.onclick = function() {
 var element=document.getElementById('mask-svg1');
-var cursor=document.getElementsByClassName('hover1');
+var hover1=document.getElementById('hover1');
+var hover2=document.getElementById('hover2');
+var hoverSVG =document.getElementById('container1');
 
 if (element.classList.contains("animation1")){
-  element.classList.replace("animation1","animation1-reverse");
+element.classList.replace("animation1","animation1-reverse");
+console.log('change cursor to shop');
+hover2.id = "hover1" ;
+hoverSVG.style.setProperty('--opacity','100%');
+
 }else if (element.classList.contains("animation1-reverse")){
-  element.classList.replace("animation1-reverse","animation1");
-  element.style = "cursor : url('../content/cursors/close.svg'),default;";
-      }else{
-  element.classList.add("animation1");
+element.classList.replace("animation1-reverse","animation1");
+hover1.id = "hover2" ;
+hoverSVG.style.setProperty('--opacity','0');
+
+console.log('change cursor to close');
+
+}else{
+element.classList.add("animation1");
+console.log('change cursor to close');
+hover1.id = "hover2" ;
+hoverSVG.style.setProperty('--opacity','0');
 }
 
-console.log('clicked')
+console.log('clicked');
 };
 
 
@@ -28,6 +41,7 @@ var element=document.getElementById('mask-svg1');
 
 if (element.classList.contains("animation1")){
   element.classList.replace("animation1","animation1-reverse");
+
 }else if (element.classList.contains("animation1-reverse")){
   element.classList.replace("animation1-reverse","animation1");
       }else{
